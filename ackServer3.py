cecb21dc8e604216fb4d84bc321f8fd8e29f13fd
faverog <63128330+faverog@users.noreply.github.com>
2023-03-21 21:56:41 -0400

    Gian better implementation

--- ackServer3.py
+++ ackServer3.py
@@ -1,11 +1,46 @@
 import socket
+import json
 
-UDP_IP = "127.0.0.1"  # localhost
-UDP_PORT = 5005
+## For Python 3
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
-sock.bind((UDP_IP, UDP_PORT))  # bind socket to IP address and port
+# Create a server socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+sock.bind(server_address)
+
+# Create the ack counter
+ack = 0
 
 while True:
-    data, addr = sock.recvfrom(1024)  # receive data (up to 1024 bytes)
-    print("Received message:", data.decode())  # print the received message
+    # receive packets from the sender
+    data, address = sock.recvfrom(4096)
+    
+    # If some data is received proceed to process it
+    if data:
+        # Decode the data
+        data = data.decode()
+        data = json.loads(data)
+
+        # Print the received data and sequence number
+        print(f"Received Data: {data['data']}")
+        print(f"Received Sequence Number: {data['seq']}")
+
+        # Check if the sequence number is the expected one
+        if data['seq'] == ack + 1:
+            ack = data['seq']
+            print("Sending ACK: " + str(ack) + "\n")
+
+        # If the sequence number is less than the expected one, reset the ack
+        elif data['seq'] == 0:
+            ack = 0
+            print("Initializing ACK...\n")
+
+        # If the sequence number is not the expected one, send the last ack
+        else:
+            print("Unexpected Sequence Number")
+            print("Sending ACK: " + str(ack) + "\n")
+        
+        # Send the ack to the client
+        sock.sendto(str(ack).encode(), address)
