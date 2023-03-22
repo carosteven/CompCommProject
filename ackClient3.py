f6b5b6b3f1c7e63653d15cfe79b79013bae9fe22
faverog <63128330+faverog@users.noreply.github.com>
2023-03-21 21:56:41 -0400

    Gian Implementation

--- ackClient3.py
+++ ackClient3.py
@@ -1,40 +1,113 @@
+import random
 import socket
-import sys
-import pickle
+import json
+import time
 
 ## For Python 3
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 server_address = ('localhost', 10000)
-message = '0'
-#'This is the message.  It will be repeated.'
-try:
-	
-    # Send data
-		sys.stderr.write('sending "%s"' % message)
-#		sent = sock.sendto(message, server_address)
-		sock.sendto(message.encode(), server_address)
+
+# Define list of messages to send
+messages = [
+    {'seq': 0, 'data': 'Start'},
+    {'seq': 1, 'data': 'Hello, World!'},
+    {'seq': 2, 'data': 'This is the second message'},
+    {'seq': 3, 'data': 'This is the third message'},
+    {'seq': 4, 'data': 'This is the fourth message'},
+    {'seq': 5, 'data': 'This is the fifth message'},
+    {'seq': 6, 'data': 'This is the sixth message'},
+    {'seq': 7, 'data': 'This is the seventh message'},
+    {'seq': 8, 'data': 'This is the eighth message'},
+    {'seq': 9, 'data': 'This is the ninth message'},
+    {'seq': 10, 'data': 'This is the tenth message'},
+    {'seq': 11, 'data': 'This is the eleventh message'},
+    {'seq': 12, 'data': 'This is the twelfth message'},
+    {'seq': 13, 'data': 'This is the thirteenth message'},
+    {'seq': 14, 'data': 'This is the fourteenth message'},
+    {'seq': 15, 'data': 'This is the fifteenth message'}    
+]
+
+# Define window size of sender
+window_size = 5
+
+# Define function to send messages
+def send_message(message):
+    message = json.dumps(message)
+
+    # Send data to server
+    sock.sendto(message.encode(), server_address)
 
     # Receive response
-		sys.stderr.write('\nwaiting to receive ack')
-		data, server = sock.recvfrom(4096)
-		
-		sys.stderr.write('I have to send "%s"' % data)
-		while str(data) != '9':
-		
-			message = data
-			sent = sock.sendto(bytes(message), server_address)
-			
-#			sent = sock.sendto(message.decode(), server_address)
+    server_ack, server = sock.recvfrom(4096)	
 
-		    
-			data, server = sock.recvfrom(4096)
-		
-			sys.stderr.write('I have to send "%s"' % data)
-		
+    return server_ack
+
+# Define function to randomly lose 20% of packets
+def packets_to_lose(start, end):
+    numbers = range(start, end)
+    packets_lost = round((end-start)*0.2)
+
+    omit = random.sample(numbers, packets_lost)
+        
+    return omit, packets_lost
+
+# Run the client
+try:
+    # Initialize ack and start index
+    server_ack = 0
+    start_index = 1
+    current_index = 1
+    stop_index = window_size
+
+    # Synchronize sender and receiver
+    print(f"Sending Data: {messages[0]['data']}")
+    print(f"Sending Sequence Number: {messages[0]['seq']}")
+    sock.sendto(json.dumps(messages[0]).encode(), server_address)
+    server_ack, server = sock.recvfrom(4096)
+    print(f"Received ACK: {int(server_ack)}\n")
+
+    # Find index of lost packets
+    omit, packets_lost = packets_to_lose(start_index, len(messages))
+    print(f"Packet(s) {omit} lost. Loss rate: ({100*packets_lost/(len(messages))}%)\n")
+
+    print("Starting transmission:\n")
+
+    # Implement Go-Back-N
+    while start_index <= len(messages) - 1:
+        if current_index not in omit:
+            print(f"Sending Data: {messages[current_index]['data']}")
+            print(f"Sending Sequence Number: {messages[current_index]['seq']}")
+            server_ack = int(send_message(messages[current_index]))
+            print(f"Received ACK: {server_ack}\n")
+        else:
+            omit.remove(current_index)
+
+        # Check if ACK is correct, increment indexes if so
+        if server_ack == start_index:
+            start_index += 1
+            stop_index = min(stop_index + 1, len(messages)-1)
+
+        # Increment current index
+        current_index += 1
+
+        # Check if all packets have been sent before timeout
+        if start_index > stop_index:
+            break
+        elif current_index > stop_index:
+            print("Reached end of Window: Timeout")
+            print(f"Expected ACK: {stop_index}")
+            print(f"Received ACK: {server_ack}")
+            print("Retransmitting from last ACK...\n")
+            current_index = start_index
+
+        time.sleep(0.5)
+
+    print("All packets received!")
+
 finally:
-    sys.stderr.write('\nclosing socket')
-raw_input()
+    # If all packets were received, close the socket
+    print('\nClosing socket')
+    
 sock.close()
