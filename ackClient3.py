8764c5e5a51144ada8e0bb9dd8b535d5f39180ad
mathewdunne <mathewdd3@gmail.com>
2023-03-21 22:53:21 -0400

    window size 10 working

--- ackClient3.py
+++ ackClient3.py
@@ -1,16 +1,59 @@
 import socket
 import time
-
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-import socket
+import random
+import json
 
 UDP_IP = "127.0.0.1"  # localhost
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
-message = "Hello, world!"  # string to send
-sock.sendto(message.encode(), (UDP_IP, UDP_PORT))  # send the string over the socket
+
+def pickPacketsToLose():
+    numbers = list(range(0, 10))
+    num1, num2 = random.sample(numbers, 2)
+    while num1 == num2:
+        num1, num2 = random.sample(numbers, 2)
+    return sorted([num1, num2])
+
+def sendPacket(message):
+    sock.sendto(json.dumps(message).encode(), (UDP_IP, UDP_PORT))
+    print(f"Sent packet {i}")
+
+    # listen for ack
+    data, addr = sock.recvfrom(1024)
+    data = data.decode()
+    data = json.loads(data)
+    return data
+
+
+packetsToLose = pickPacketsToLose()
+
+# main loop
+lastAck = -1
+for i in range(0, 10):
+    message = {"seq": i, "data": f"This is message {i}"}
+
+    if i in packetsToLose:
+        print(f"Packet {i} lost (not sent)")
+
+    else:
+        # send packet
+        response = sendPacket(message)
+
+        # check if ack is correct
+        lastAck = int(response['ack'])
+        print(f"Received ack {response['ack']}")
+
+print("\n")
+
+# resend lost packets
+for i in range(lastAck+1, 10):
+    message = {"seq": i, "data": f"This is message {i}"}
+    response = sendPacket(message)
+    # check if ack is correct
+    if int(response['ack']) == lastAck + 1:
+        lastAck += 1
+    print(f"Received ack {response['ack']}")
+
+if lastAck == 9:
+    print("All packets received")
--- ackServer3.py
+++ ackServer3.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 UDP_IP = "127.0.0.1"  # localhost
 UDP_PORT = 5005
@@ -6,6 +7,22 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
 sock.bind((UDP_IP, UDP_PORT))  # bind socket to IP address and port
 
+lastCorrectPacket = -1
+
 while True:
     data, addr = sock.recvfrom(1024)  # receive data (up to 1024 bytes)
-    print("Received message:", data.decode())  # print the received message
+    data = data.decode()
+    data = json.loads(data)
+    print("Received:", data['data'])  # print the received message
+
+    # send ack back to sender
+    if data['seq'] == lastCorrectPacket + 1:
+        lastCorrectPacket += 1
+
+    ack = {"ack": lastCorrectPacket}
+    sock.sendto(json.dumps(ack).encode(), addr)
+    print("Sent ack", ack['ack'])
+
+    if lastCorrectPacket == 9:
+        print("All packets received")
+        break
