d008974857b03ffa31f19bf3045c7ecce35259f6
Steven Caro <caros@uwindsor.ca>
2023-03-21 23:51:38 -0400

    Variable window size

--- ackClient3.py
+++ ackClient3.py
@@ -30,30 +30,47 @@ packetsToLose = pickPacketsToLose()
 
 # main loop
 lastAck = -1
-for i in range(0, 10):
-    message = {"seq": i, "data": f"This is message {i}"}
+windowSize = 5
+windowEnd = windowSize
+messageLen = 10
 
-    if i in packetsToLose:
-        print(f"Packet {i} lost (not sent)")
+while True:
+    if lastAck < windowEnd-1:
+        windowStart = lastAck + 1
+        if windowStart + windowSize < 10:
+            windowEnd = windowStart + windowSize
+        else:
+            windowEnd = 10
 
-    else:
-        # send packet
+    for i in range(windowStart, windowEnd):
+        message = {"seq": i, "data": f"This is message {i}"}
+
+        if i in packetsToLose:
+            print(f"Packet {i} lost (not sent)")
+            packetsToLose.remove(i)
+
+        else:
+            # send packet
+            response = sendPacket(message)
+
+            # check if ack is correct
+            lastAck = int(response['ack'])
+            print(f"Received ack {response['ack']}")
+    
+    if lastAck == messageLen-1:
+        break
+
+    print("\n")
+
+'''# resend lost packets
+if lastAck != windowSize-1:
+    for i in range(lastAck+1, 10):
+        message = {"seq": i, "data": f"This is message {i}"}
         response = sendPacket(message)
-
         # check if ack is correct
-        lastAck = int(response['ack'])
-        print(f"Received ack {response['ack']}")
-
-print("\n")
-
-# resend lost packets
-for i in range(lastAck+1, 10):
-    message = {"seq": i, "data": f"This is message {i}"}
-    response = sendPacket(message)
-    # check if ack is correct
-    if int(response['ack']) == lastAck + 1:
-        lastAck += 1
-    print(f"Received ack {response['ack']}")
+        if int(response['ack']) == lastAck + 1:
+            lastAck += 1
+        print(f"Received ack {response['ack']}")'''
 
 if lastAck == 9:
     print("All packets received")
