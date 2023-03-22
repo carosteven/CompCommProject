afaaf408e10d5ab580bb4fce2360a3296cba58cf
faverog <63128330+faverog@users.noreply.github.com>
2023-03-21 22:37:44 -0400

    Communication between server and client

--- ackClient3.py
+++ ackClient3.py
@@ -1,6 +1,6 @@
 import socket
 import sys
-import pickle
+import json
 
 ## For Python 3
 
@@ -8,22 +8,31 @@ import pickle
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = ('localhost', 10000)
-message = '0'
-try:
-    # Send data
-	sys.stderr.write('sending "%s"' % message)
-	sock.sendto(message.encode(), server_address)
+messages = [
+    {'seq': 1, 'data': 'Hello, World!'},
+    {'seq': 2, 'data': 'This is the second message'},
+    {'seq': 3, 'data': 'This is the third message'},
+    {'seq': 4, 'data': 'This is the fourth message'},
+    {'seq': 5, 'data': 'This is the fifth message'},
+    {'seq': 6, 'data': 'This is the sixth message'},
+    {'seq': 7, 'data': 'This is the seventh message'},
+    {'seq': 8, 'data': 'This is the eighth message'},
+    {'seq': 9, 'data': 'This is the ninth message'},
+    {'seq': 10, 'data': 'This is the tenth message'}    
+]
 
-    # Receive response
-	sys.stderr.write('\nwaiting to receive ack')
-	data, server = sock.recvfrom(4096)
-	
-	sys.stderr.write('I have to send "%s"' % data)
-	while str(data) != '9':
-		message = data
-		sent = sock.sendto(bytes(message), server_address)
-		data, server = sock.recvfrom(4096)
-		sys.stderr.write('I have to send "%s"' % data)
+try:
+    server_ack = 0
+
+    # Send data, intentionally lose 6th and 9th packets
+    for message in messages:
+        message = json.dumps(message)
+        sock.sendto(message.encode(), server_address)
+
+        # Receive response
+        server_ack, server = sock.recvfrom(4096)	
+
+        print('Received: ' + str(server_ack.decode()))
 		
 finally:
     sys.stderr.write('\nclosing socket')
--- ackServer3.py
+++ ackServer3.py
@@ -1,5 +1,5 @@
 import socket
-import sys
+import sys, json
 
 ## For Python 3
 
@@ -8,22 +8,21 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
 server_address = ('localhost', 10000)
-sys.stderr.write('starting up on %s port %s' % server_address)
 sock.bind(server_address)
+
+# Create the byte counter
+ack = 0
 
 while True:
     # receive packets from the sender
-    sys.stderr.write('\nwaiting to receive message')
     data, address = sock.recvfrom(4096)
 
-    sys.stderr.write('received %s bytes from %s' % (len(data), address))
-    sys.stderr.write(str(data))
-    
-    m = ord(data) + 1
-    
-    print("\ndata, m = ",data,m)
+    print(str(data.decode()))
     
     if data:
-        sent = sock.sendto(bytes(m), address)
-        print("chr(m) = ",chr(m))
-        sys.stderr.write('sent %s bytes back to %s' % (sent, address))
+        data = data.decode()
+        data = json.loads(data)
+        if data['seq'] == ack + 1:
+            ack = data['seq']
+        
+        sock.sendto(str(ack).encode(), address)
